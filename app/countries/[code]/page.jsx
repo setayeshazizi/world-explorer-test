e55bff71de6350ff5cbad74b92466abbe2ba8f22
Satayesh Esmaily <setayeshesmaily59@gmail.com>
2026-05-29 15:02:23 +0430

    DETAILS PAGE

--- app/countries/[code]/page.jsx
+++ app/countries/[code]/page.jsx
@@ -1,18 +1,18 @@
-// app/countries/[code]/page.jsx
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default async function CountryDetailsPage({ params }) {
-  const { code } = params;
+  const resolvedParams = await params;
+  const code = resolvedParams?.code;
 
-  console.log("Country code:", code);
+  if (!code) {
+    notFound();
+  }
 
   try {
     const res = await fetch(`https://restcountries.com/v3.1/alpha/${code}`, {
       cache: "no-store",
     });
-
-    console.log("Response status:", res.status); 
 
     if (!res.ok) {
       notFound();
@@ -30,12 +30,12 @@ export default async function CountryDetailsPage({ params }) {
         <Link href="/countries">← Back to Countries</Link>
 
         <h1>{country.name?.common}</h1>
-        <img src={country.flags?.png} alt={country.name?.common} />
+        <img src={country.flags?.png || country.flags?.svg} alt={country.name?.common} />
         <p>Capital: {country.capital?.[0] || "No capital"}</p>
         <p>Region: {country.region}</p>
         <p>Population: {country.population?.toLocaleString()}</p>
 
-        <a href={country.maps?.googleMaps} target="_blank">
+        <a href={country.maps?.googleMaps} target="_blank" rel="noreferrer">
           View on Google Maps
         </a>
       </main>
